693ca224441baa149861b95516c3b00e8bd44afd
Nivaldo Ramos <nramosj7@gmail.com>
2018-04-10 18:29:07 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/nramos/cursomc/CursomcApplication.java
+++ src/main/java/com/nramos/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.nramos.cursomc.domain.Cidade;
 import com.nramos.cursomc.domain.Cliente;
 import com.nramos.cursomc.domain.Endereco;
 import com.nramos.cursomc.domain.Estado;
+import com.nramos.cursomc.domain.ItemPedido;
 import com.nramos.cursomc.domain.Pagamento;
 import com.nramos.cursomc.domain.PagamentoComBoleto;
 import com.nramos.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.nramos.cursomc.repositories.CidadeRepository;
 import com.nramos.cursomc.repositories.ClienteRepository;
 import com.nramos.cursomc.repositories.EnderecoRepository;
 import com.nramos.cursomc.repositories.EstadoRepository;
+import com.nramos.cursomc.repositories.ItemPedidoRepository;
 import com.nramos.cursomc.repositories.PagamentoRepository;
 import com.nramos.cursomc.repositories.PedidoRepository;
 import com.nramos.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -102,6 +106,17 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
@@ -110,5 +125,6 @@ public class CursomcApplication implements CommandLineRunner{
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
